58e59cb4ed1e546dc955413c5d49cb0d580ab12b
danieleiudica <daniele.iudica@hotmail.it>
2023-08-10 16:32:21 +0200

    add training tab group

--- src/app/training/training/training.component.ts
+++ src/app/training/training/training.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+// import { TrainingService } from "./training.service";
+import { Subscription } from "rxjs";
 
 @Component({
   selector: 'app-training',
   templateUrl: './training.component.html',
   styleUrls: ['./training.component.scss']
 })
-export class TrainingComponent {
+export class TrainingComponent implements OnInit {
+   onGoingTraining = false;
+    exerciseSubscription!: Subscription;
+    // constructor(private trainingSrv: TrainingService) {}
 
+    ngOnInit(): void {
+        // this.exerciseSubscription = this.trainingSrv.exerciseChanged.subscribe((exercise) => {
+        //     exercise ? (this.onGoingTraining = true) : (this.onGoingTraining = false);
+        // });
+    }
 }
